9a81b89fbb36e4a58e64ff1427a9e7df48d52800
Shamir Maharjan <shamirmaharjan07@gmail.com>
2025-08-02 14:47:20 +0545

    add gracefull shutdown

--- cmd/Student-Management-System/main.go
+++ cmd/Student-Management-System/main.go
@@ -1,10 +1,68 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/ShamirMaharjan/Student-management-system--GO/internal/config"
 )
 
 func main() {
-	fmt.Println("Hello student system")
 
+	//load config
+	cfg := config.MustLoad()
+
+	//setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello student system"))
+	})
+
+	//setup server
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	slog.Info("Server started at port", slog.String("port", cfg.Addr))
+
+	//using channel to synchronize the server like a server stop garyo re tara euta event ko executation complete vako xaina re
+	//so tyo event ko executation complete na vaye samma main go routine stop hundaina as channel le block garira hunxa
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatalf("failed to start server: %v", err)
+		}
+	}()
+
+	<-done
+
+	//kaile kai server stop hundaina infinitly run vairakhxa tei vayera hamle context use gareko
+	//so if server 5 second samma stop vayena vani server stop vayena vanera msg auxa
+	//natra server shutdown hunxa
+	slog.Info("Shutting down server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
+
+	if err != nil {
+		slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("Server Shutdown Gracefully")
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -9,12 +9,12 @@ import (
 )
 
 type HTTPServer struct {
-	Addr string
+	Addr string `yaml:"address" env-required:"true"`
 }
 
 type Config struct {
-	Env         string `yaml:"env" env:"ENV" env-reqired:"true"`
-	StoragePath string `yaml:"storage_path" env-reqired:"true"`
+	Env         string `yaml:"env" env:"ENV" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
 	HTTPServer  `yaml:"http_server"`
 }
 
